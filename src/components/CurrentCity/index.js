eee400696ecd446b6467fd66867dc0bb0ecdbf60
lxf101 <lixinfang101@126.com>
2024-05-28 20:39:30 +1200

    finish weather-app

--- src/components/CurrentCity/index.js
+++ src/components/CurrentCity/index.js
@@ -1,7 +1,21 @@
+import currentBg from '../../assets/background/Hail_background.png'
+import rainImg from '../../assets/weather_icon/Rain.png'
+import DetailInfo from '../DetailInfo'
 
 function CurrentCity(){
     return (
-        <div className="w-[270px] my-5 mx-5 bg-gradient-to-b from-[#8091e4] to-[#5a52d4] rounded-3xl" style={{height: `calc(100% - 40px)`}}>CurrentCity </div>
+        <div className="w-[270px] my-5 mx-5 bg-gradient-to-b from-[#8091e4] to-[#5a52d4] rounded-3xl" style={{height: `calc(100% - 40px)`}}>
+            <div className="relative w-full h-full" style={{backgroundImage: `url(${currentBg})`}}>
+                <p className='text-sm text-white pt-3 pl-4'>23 July, Sunday 12:00</p>
+                <div className='flex flex-col items-center'>
+                    <h2 className='text-center mt-[25px] text-white font-bold text-[25px]'>London</h2>
+                    <h1 className='mt-5 text-center text-white font-bold text-[60px]'>0</h1>
+                    <h3 className='text-center text-white text-sm'>0~2°</h3>
+                    <img className="w-[120px]" src={rainImg} alt="rain"></img>
+                </div>
+                <DetailInfo />
+            </div>
+        </div>
     )
 }
 
---
+++ src/components/DetailInfo/index.js
@@ -0,0 +1,31 @@
+import shuidi from '../../assets/icon/shuidi.png'
+import fengxiang from '../../assets/icon/fengxiang.png'
+import shidu from '../../assets/icon/shidu.png'
+import wenduji from '../../assets/icon/wenduji.png'
+
+function DetailInfo(){
+    return (
+        <div className="absolute bottom-[15px] w-[230px] h-[98px] mx-3 bg-[#ebeefb] rounded-3xl">
+            <ul className="w-full h-full flex justify-between items-center">
+                <li className="flex-1 flex flex-col items-center">
+                    <img className="w-[20px] h-[20px]" src={shuidi} alt="shuidi"></img>
+                    <span className='text-sm text-[#414161] mt-1'>85%</span>
+                </li>
+                <li className="flex-1 flex flex-col items-center">
+                    <img className="w-[20px] h-[20px]" src={fengxiang} alt="fengxiang"></img>
+                    <span className='text-sm text-[#414161] mt-1'>9km/h</span>
+                </li>
+                <li className="flex-1 flex flex-col items-center">
+                    <img className="w-[20px] h-[20px]" src={shidu} alt="shidu"></img>
+                    <span className='text-sm text-[#414161] mt-1'>75ug</span>
+                </li>
+                <li className="flex-1 flex flex-col items-center">
+                    <img className="w-[20px] h-[20px]" src={wenduji} alt="wenduji"></img>
+                    <span className='text-sm text-[#414161] mt-1'>26°</span>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default DetailInfo
